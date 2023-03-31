aa2efbf5db8bc509d3d1f8cd643113210d869464
gh120515 <123872197+gh120515@users.noreply.github.com>
2023-03-31 19:43:11 +1030

    cleaned up starter variables & initial generation prompt.

--- Develop/script.js
+++ Develop/script.js
@@ -1,100 +1,99 @@
 // Assignment code here
 
+// TODO:
+// concatonation
+// validation - to see if all the requests user checked is included
 
-// Get references to the #generate element
+// Get references to the #generate & #password element
 var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
+var passwordText = document.querySelector("#password");
 
 // variables
 
-let includeLowerCase
-let includeUpperCase
-let includeNumbers
-let includeSymbols
+let includeLowerCase;
+let includeUpperCase;
+let includeNumbers;
+let includeSymbols;
 
-let generatedPassword = ""
-let passwordLength = ""
+let generatedPassword = "";
 
-// character types & random generation function
 
-// let lowerCaseChar = "abcdefghijklmnopqrstuvwxyz";
-// let upperCaseChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-// let numberChar = "0123456789";
-// let specialChar = "~!@#$%^&*-_+=(){}[]¥;:'<>,./?";
+// character types (converted into arrays using 'split' function)
+
+const lowerCaseChar = "abcdefghijklmnopqrstuvwxyz";
+const upperCaseChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const numberChar = "0123456789";
+const specialChar = "~!@#$%^&*-_+=(){}[]¥;:'<>,./?";
+
+const lowerCase = lowerCaseChar.split('');
+const upperCase = upperCaseChar.split('');
+const numbers = numberChar.split('');
+const special = specialChar.split('');
+
 
 // functions for randomising characters based on the chosen password length in the previous prompt
 function getRandomLower() {
-  const lowerCase = "abcdefghijklmnopqrstuvwxyz";
   return lowerCase[Math.floor(Math.random() * lowerCase.length)];
 }
 
 function getRandomUpper() {
-  const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   return upperCase[Math.floor(Math.random() * upperCase.length)];
 }
 
 function getRandomNumber() {
-  const number = "0123456789";
-  return number[Math.floor(Math.random() * number.length)];
+  return numbers[Math.floor(Math.random() * number.length)];
 }
 
 function getRandomSymbol() {
-  const symbols = "~!@#$%^&*-_+=(){}[]¥;:'<>,./?";
   return symbols[Math.floor(Math.random() * symbols.length)];
 }
 
 // password generation
 
-writePassword = function() {
+function generatePassword() {
   // Ask user for their choice
-  let userLength = window.prompt("Select your password length (by entering a number between 8-128 characters)");
+  var userLength = window.prompt("Select your password length (by entering a number between 8-128 characters)");
 
-  // If user pressed Cancel, immediately end function
-  if (!userLength) {
+  // // If user pressed Cancel or entered non-numerical characters, end function
+  if (userLength < 8 || userLength > 128 || isNaN || !userLength) {
+    alert("Please enter numerical character(s) only & ensure it is within 8-168 characters.");
     return;
-  }
-  let passwordLength = userLength;
-
-  generatePassword();
-
-  generatePassword = function() {
-
-    for (i = 0, i <= passwordLength; i++;) {
-
-    let userLowerCase = window.confirm("Would you like to include lower case characters?");
-    if (userLowerCase === true) {
-      generatedPassword += getRandomLower();
-    }
+  };
   
-      let userUpperCase = window.confirm("Would you like to include upper case characters?");
-    if (userUpperCase === true) {
-      generatedPassword += getRandomUpper();
-    }
+}
+    // WIP
 
-  let userNumbers = window.confirm("Would you like to include numerical characters?");
-  if (userNumbers === true) {
-    generatedPassword += getRandomNumber();
-  }
+  //   for (i = 0, i <= userLength; i++;) {
 
-  let userSpecial = window.confirm("Would you like to include special characters? (e.g. !@#$%)");
-  if (userSpecial === true) {
-   generatedPassword += getRandomSymbol();
-  }
-
-  let password = generatedPassword.slice(0, length);
-
-  return password; 
+  //   let userLowerCase = window.confirm("Would you like to include lower case characters?");
+  //   if (userLowerCase === true) {
+  //     generatedPassword += getRandomLower();
+  //   }
   
-  }
-};
+  //     let userUpperCase = window.confirm("Would you like to include upper case characters?");
+  //   if (userUpperCase === true) {
+  //     generatedPassword += getRandomUpper();
+  //   }
+
+  // let userNumbers = window.confirm("Would you like to include numerical characters?");
+  // if (userNumbers === true) {
+  //   generatedPassword += getRandomNumber();
+  // }
+
+  // let userSpecial = window.confirm("Would you like to include special characters? (e.g. !@#$%)");
+  // if (userSpecial === true) {
+  //  generatedPassword += getRandomSymbol();
+  // }
+
+  // }
+// };
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+
+  passwordText.value = password;
+}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
